83da74f827794357fbf480329785b035adf55e55
clacktronics <ben@clacktronics.co.uk>
2016-11-15 07:09:14 +0000

    array of past images made

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import urllib
 import StringIO
 
 class camera(object):
-    def __init__(self,url):
+    def __init__(self,url,buffer):
         self.current = Image.new("L", (640,648) ,0)
         self.last = None
         self.url = url
+	self.buffer_len = buffer
+	self.buffer = []
         self.updateImage()
 
 
     def updateImage(self):
-        self.last = self.current.copy()
-        stream = urllib.urlopen(self.url)
+	self.buffer.append(self.getImage())
+        if len(self.buffer) > self.buffer_len:
+		self.buffer.pop()
+	#self.last = self.current.copy()
+        
+    def getImage(self):
+	stream = urllib.urlopen(self.url)
         bytes=''
         while True:
             bytes+=stream.read(1024)
@@ -21,12 +28,12 @@ class camera(object):
             if a!=-1 and b!=-1:
                 jpg = bytes[a:b+2]
                 bytes= bytes[b+2:]
-                self.current = Image.open(StringIO.StringIO(jpg))
-                break
+                return Image.open(StringIO.StringIO(jpg))
 
 from time import sleep
+from subprocess import call
 
-camera1 = camera("http://192.168.1.126:81/videostream.cgi?user=admin&pwd=&resolution=32&rate=0")
+camera1 = camera("http://192.168.1.112:81/videostream.cgi?user=admin&pwd=&resolution=16&rate=0",10)
 
 i=0
 
@@ -34,9 +41,16 @@ canvas = Image.new("L",(2480,3508),255)
 
 while True:
     camera1.updateImage()
-    canvas.paste(camera1.current,((2480/2)-640-20,10))
-    canvas.paste(camera1.last,((2480/2)+20,10))
-    canvas.save("image%03d.jpg" % i)
-    print i
     i+=1
+    print "----"
+    for l in camera1.buffer:
+	print l
+    print "----"
+    if i > 10:
+	print "a"
+    	canvas.paste(camera1.buffer[-1],((2480/2)-640-20,10))
+    	canvas.paste(camera1.buffer[0],((2480/2)+20,10))
+    	canvas.save("image.jpg")
+	i = 0
+	#call(['lp','image.jpg'])
     sleep(1)
