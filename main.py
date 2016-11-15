ec81b663da9539b82f7827aec2ee01e1d438e19a
clacktronics <clacktronics@gmail.com>
2016-11-15 07:51:23 +0000

    Added timer and text to image

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import urllib
 import StringIO
+from time import gmtime, strftime
+from timeit import default_timer as timer
+
 
 class camera(object):
     def __init__(self,url,buffer):
         self.current = Image.new("L", (640,648) ,0)
         self.last = None
         self.url = url
-	self.buffer_len = buffer
-	self.buffer = []
+        self.buffer_len = buffer
+        self.buffer = []
         self.updateImage()
 
 
     def updateImage(self):
-	self.buffer.append(self.getImage())
+        self.buffer.append([strftime("%Y-%m-%d %H:%M:%S", gmtime()), self.getImage()])
         if len(self.buffer) > self.buffer_len:
-		self.buffer.pop()
-	#self.last = self.current.copy()
-        
+            self.buffer.pop(0)
+            #self.last = self.current.copy()
+
     def getImage(self):
-	stream = urllib.urlopen(self.url)
+        stream = urllib.urlopen(self.url)
         bytes=''
         while True:
             bytes+=stream.read(1024)
@@ -33,24 +36,43 @@ class camera(object):
 from time import sleep
 from subprocess import call
 
-camera1 = camera("http://192.168.1.112:81/videostream.cgi?user=admin&pwd=&resolution=16&rate=0",10)
+camera1 = camera("http://192.168.1.112:81/videostream.cgi?user=admin&pwd=&resolution=32&rate=0",10)
 
 i=0
 
-canvas = Image.new("L",(2480,3508),255)
+last_time = timer()
 
 while True:
-    camera1.updateImage()
-    i+=1
-    print "----"
-    for l in camera1.buffer:
-	print l
-    print "----"
-    if i > 10:
-	print "a"
-    	canvas.paste(camera1.buffer[-1],((2480/2)-640-20,10))
-    	canvas.paste(camera1.buffer[0],((2480/2)+20,10))
-    	canvas.save("image.jpg")
-	i = 0
-	#call(['lp','image.jpg'])
-    sleep(1)
+
+    if (timer() - last_time) > 1:
+        camera1.updateImage()
+        i+=1
+        print camera1.buffer[-1][0]
+        last_time = timer()
+
+    # print "----"
+    # for l in camera1.buffer:
+    # print l
+    # print "----"
+
+    if i > 5:
+        print "export"
+        canvas = Image.new("L",(2480,3508),255)
+        draw = ImageDraw.Draw(canvas)
+        fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+
+        image_w, image_h = camera1.buffer[-1][1].size
+        canvas_w, canvas_h = canvas.size
+        centre_w = canvas_w/2
+
+        canvas.paste(camera1.buffer[0][1],(centre_w-image_w-100,100))
+        canvas.paste(camera1.buffer[-1][1],(centre_w+100,100))
+
+        draw.text((centre_w-image_w-100,image_h+100), camera1.buffer[0][0], font=fnt)
+        draw.text((centre_w+100,image_h+100), camera1.buffer[-1][0], font=fnt)
+
+        canvas.save("image.jpg")
+        del draw
+        del canvas
+        i = 0
+        #call(['lp','image.jpg'])
